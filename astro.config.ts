d877b8320e4e21c8342c47ac8f71d4fb90984abd
Aksara Sukma <fajar.aditiya@icloud.com>
2025-06-25 03:40:00 +0800

    update web

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,7 @@ import {
 } from "@shikijs/transformers";
 import { transformerFileName } from "./src/utils/transformers/fileName";
 import { SITE } from "./src/config";
+import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,7 +21,7 @@ export default defineConfig({
     }),
   ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
+    remarkPlugins: [remarkToc, remarkReadingTime, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
       // For more themes, visit https://shiki.style/themes
       themes: { light: "min-light", dark: "night-owl" },
@@ -41,9 +42,8 @@ export default defineConfig({
     },
   },
   image: {
-    // Used for all Markdown images; not configurable per-image
-    // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: "constrained",
+    responsiveStyles: true,
+    layout: "constrained",
   },
   env: {
     schema: {
@@ -55,7 +55,6 @@ export default defineConfig({
     },
   },
   experimental: {
-    responsiveImages: true,
     preserveScriptOrder: true,
   },
 });
